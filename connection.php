6a65d43be5380d9e6efdc21c8efdcd3836e45d2c
AGayMan69 <scjohnwoo123@gmail.com>
2022-06-15 18:47:02 +0800

    update project

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname = "127.0.0.1";
-$username = "root";
-$password = "";
-$database = "projectdb";
-$con = mysqli_connect($hostname, $username, $password, $database) or die(mysqli_connect_error());
-?>
--- placeOrder.php
+++ placeOrder.php
@@ -6,8 +6,8 @@
 
 session_start();
 
-include "connection.php";
-include "nav.php";
+include "database_connection.php";
+
 
 
 //Add item to cart
@@ -77,154 +77,169 @@ if(isset($_POST['qtyChange'])){
     var_dump($_POST);
 }
 ?>
-<div class="container-fluid" xmlns="http://www.w3.org/1999/html">
-        <div class="col-md-12">
-            <div class="row text-center">
-                <div class="col-md-6">
-                    <h2 class="text-center py-3">Item List</h2>
-                    <div class="col-md-12">
-                        <div class="row">
-                    <?php
-                    $query ="SELECT * FROM item";
-                    $rs = mysqli_query($con,$query);
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php include "nav.php";?>
+<div class="container" xmlns="http://www.w3.org/1999/html">
+    <div class="col-md-12">
+        <div class="row text-center">
+            <div class="col-md-6">
+                <h2 class="text-center py-3">Item List</h2>
+                <div class="col-md-12">
+                    <div class="row">
+                        <?php
+                        $query ="SELECT * FROM item";
+                        $conn = getDBconnection();
+                        $rs = mysqli_query($conn,$query);
 
-                    while ($row = mysqli_fetch_assoc($rs)){
-                        // $itemID $itemName $itemDescription $stockQuantity $price
-                        extract($row);
-                        ?>
+                        while ($row = mysqli_fetch_assoc($rs)){
+                            // $itemID $itemName $itemDescription $stockQuantity $price
+                            extract($row);
+                            ?>
 
-                        <!--CARD-->
-                        <div class="col-lg-4 col-md-6 col-sm-12 d-flex align-self-stretch py-2">
-                            <div class="card shadow-sm mb-4">
-                                <!--Product Image-->
-                                <img src="images/products/<?=$itemID?>.png" class="card-img-top" alt="">
-                                <div class="card-body d-flex flex-column">
-                                    <!--Product Name-->
-                                    <h6 class="card-title text-uppercase"><?=$itemName?></h6>
-                                    <p class="text-muted"></p>
-                                    <div class="mt-auto border border-white ">
-                                        <!--Price tag-->
-                                        <p class="text-uppercase mb-0 py-2">HK$<?=$price?></p>
+                            <!--CARD-->
+                            <div class="col-lg-4 col-md-6 col-sm-12 d-flex align-self-stretch py-2">
+                                <div class="card shadow-sm mb-4">
+                                    <!--Product Image-->
+                                    <img src="images/products/<?=$itemID?>.png" class="card-img-top" alt="">
+                                    <div class="card-body d-flex flex-column">
+                                        <!--Product Name-->
+                                        <h6 class="card-title text-uppercase"><?=$itemName?></h6>
+                                        <p class="text-muted"></p>
+                                        <div class="mt-auto border border-white ">
+                                            <!--Price tag-->
+                                            <p class="text-uppercase mb-0 py-2">HK$<?=$price?></p>
 
-                                        <!--Page anchor #form-anchor id="form-anchor"-->
-                                        <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>" >
+                                            <!--Page anchor #form-anchor id="form-anchor"-->
+                                            <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>" >
 
-                                        <!--Hidden element-->
-                                        <input type="hidden" name="itemID" value="<?=$itemID?>">
-                                        <input type="hidden" name="itemName" value="<?=$itemName?>">
-                                        <input type="hidden" name="price" value="<?=$price?>">
-                                        <input type="hidden" name="stockQuantity" value="<?=$stockQuantity?>">
-                                        <!--Hidden element-->
+                                                <!--Hidden element-->
+                                                <input type="hidden" name="itemID" value="<?=$itemID?>">
+                                                <input type="hidden" name="itemName" value="<?=$itemName?>">
+                                                <input type="hidden" name="price" value="<?=$price?>">
+                                                <input type="hidden" name="stockQuantity" value="<?=$stockQuantity?>">
+                                                <!--Hidden element-->
 
-                                        <!--Submit button-->
-                                        <input type="submit" class="btn btn-warning " name="addToCart" value="Add To Cart">
-                                            <i class="fas fa-shopping-cart"></i>
-                                            </input>
-                                        </form>
+                                                <!--Submit button-->
+                                                <input type="submit" class="btn btn-warning " name="addToCart" value="Add To Cart">
+                                                <i class="fas fa-shopping-cart"></i>
+                                                </input>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php
-                    }
-                    ?>
-                        </div>
+                            <?php
+                        }
+                        ?>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <!--Card top margin-->
-                    <div class="py-2"></div>
+            </div>
+            <div class="col-md-6">
+                <!--Card top margin-->
+                <div class="py-2"></div>
 
-                        <div class="card py-3">
-                            <div class="d-flex flex-row justify-content-start p-4">
-                                <span><h1>Shopping Cart</h1></span>
-                            </div>
+                <div class="card py-3">
+                    <div class="d-flex flex-row justify-content-start p-4">
+                        <span><h1>Shopping Cart</h1></span>
+                    </div>
 
-                            <div class="table-responsive">
-                                <table class="table table-bordered">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th class="w-40">Name</th>
+                                <th class="w-5">Quantity</th>
+                                <th class="w-20">Price</th>
+                                <th class="w-20">Total</th>
+                                <th class="w-5">&nbsp</th>
+                            </tr>
+
+
+                            <?php
+                            if(!empty($_SESSION['cart']))
+                            {
+                                $amount=0;
+                                foreach ($_SESSION['cart'] as $key => $values){
+                                    extract($values);
+                                    // $itemID $itemName $itemDescription $stockQuantity $price
+                                    ?>
                                     <tr>
-                                        <th class="w-40">Name</th>
-                                        <th class="w-5">Quantity</th>
-                                        <th class="w-20">Price</th>
-                                        <th class="w-20">Total</th>
-                                        <th class="w-5">&nbsp</th>
+                                        <!--Item Name & hidden itemID -->
+                                        <td>
+                                            <?=$itemName?>
+                                        </td>
+
+                                        <!--customer buy qty-->
+                                        <td>
+
+                                            <input type="hidden"  name="itemID" value="<?=$itemID?>">
+                                            <input type="number" class="itemqty" name="buyqty" min="1" max="<?=$stockQuantity?>" value="1" onclick="calAmount()" onchange="getValue(<?=$itemID?>,this)">
+
+                                        </td>
+
+                                        <!--unit price-->
+                                        <td>HK$<?=$price?>
+                                            <input type="hidden" class="itemprice" value="<?=$price?>">
+                                        </td>
+
+                                        <!--total price =  buyqty * unit price-->
+                                        <td class="itemamount">
+                                            0
+                                        </td>
+
+                                        <!--REMOVE button-->
+                                        <td>
+                                            <input type="hidden" name="removeitemID" value="<?=$itemID?>">
+                                            <input type="submit" name="removeItem" class="btn btn-sm btn-danger btn-block" value="REMOVE">
+                                        </td>
                                     </tr>
-
-
-                                        <?php
-                                        if(!empty($_SESSION['cart']))
-                                        {
-                                            $amount=0;
-                                            foreach ($_SESSION['cart'] as $key => $values){
-                                                extract($values);
-                                                // $itemID $itemName $itemDescription $stockQuantity $price
-                                                ?>
-                                                <tr>
-                                                    <!--Item Name & hidden itemID -->
-                                                    <td>
-                                                        <?=$itemName?>
-                                                    </td>
-
-                                                    <!--customer buy qty-->
-                                                    <td>
-
-                                                            <input type="hidden"  name="itemID" value="<?=$itemID?>">
-                                                            <input type="number" class="itemqty" name="buyqty" min="1" max="<?=$stockQuantity?>" value="1" onclick="calAmount()" onchange="getValue(<?=$itemID?>,this)">
-
-                                                    </td>
-
-                                                    <!--unit price-->
-                                                    <td>HK$<?=$price?>
-                                                        <input type="hidden" class="itemprice" value="<?=$price?>">
-                                                    </td>
-
-                                                    <!--total price =  buyqty * unit price-->
-                                                    <td class="itemamount">
-                                                        0
-                                                    </td>
-
-                                                    <!--REMOVE button-->
-                                                    <td>
-                                                        <input type="hidden" name="removeitemID" value="<?=$itemID?>">
-                                                        <input type="submit" name="removeItem" class="btn btn-sm btn-danger btn-block" value="REMOVE">
-                                                    </td>
-                                                </tr>
-                                                <?php
-                                            }
-                                        } ?>
-                                </table>
+                                    <?php
+                                }
+                            } ?>
+                        </table>
 
 
 
 
-                                <!-- Display total price-->
-                                <div class="order">
-                                    <div class="d-flex flex-row justify-content-between p-3 mx-3">
-                                        <span>Subtotal</span>
-                                        <span  id="subTotalSpan">XXX</span>
-                                    </div>
-
-                                    <div class="d-flex flex-row justify-content-between p-3 mx-3">
-                                        <span>Discount</span>
-                                        <span>-0</span>
-                                    </div>
-
-                                    <div class="d-flex flex-row justify-content-between p-3 mx-3">
-                                        <span>Total</span>
-                                        <span>0</span>
-                                    </div>
-                                    <div class="d-flex flex-row justify-content-end p-5">
-                                        <button class="btn btn-primary mx-2">Enter Shipment Details</button>
-                                        <input type="submit" name="checkout" class="btn btn-primary" value="Proceed to Payment">
-                                    </div>
-                                </div>
+                        <!-- Display total price-->
+                        <div class="order">
+                            <div class="d-flex flex-row justify-content-between p-3 mx-3">
+                                <span>Subtotal</span>
+                                <span  id="subTotalSpan">XXX</span>
                             </div>
-                            <!-- Display total price-->
-                   </div>
+
+                            <div class="d-flex flex-row justify-content-between p-3 mx-3">
+                                <span>Discount</span>
+                                <span>-0</span>
+                            </div>
+
+                            <div class="d-flex flex-row justify-content-between p-3 mx-3">
+                                <span>Total</span>
+                                <span>0</span>
+                            </div>
+                            <div class="d-flex flex-row justify-content-end p-5">
+                                <button class="btn btn-primary mx-2">Enter Shipment Details</button>
+                                <input type="submit" name="checkout" class="btn btn-primary" value="Proceed to Payment">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Display total price-->
                 </div>
             </div>
         </div>
     </div>
+</div>
+</body>
+</html>
+
 
 
 
