8976cb95ac92a7394842c248f5a753a049036e7c
AGayMan69 <scjohnwoo123@gmail.com>
2022-06-15 18:18:35 +0800

    update navbar

--- nav.php
+++ nav.php
@@ -1,83 +1,65 @@
+<?php
+
+$page = "Staff";
+if ($page === "Staff") {
+   $urls = array(
+       "View Order" => 'viewOrder.php',
+       "Create Order" => 'placeOrder.php'
+   );
+} else {
+    $urls = array(
+        "Items" => 'items.php',
+        "Reports" => 'salesReport.php',
+        "Customers" => 'customers.php'
+    );
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>Order</title>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    <script>
-        //Return an array which hold all the element from shopping cart
-        var itemprice = document.getElementsByClassName('itemprice');
-        var itemqty = document.getElementsByClassName('itemqty');
-        var itemamount = document.getElementsByClassName('itemamount');
-        var subTotal = 0;
-
-        function calAmount(){
-            subTotal=0;
-            for (i=0;i<itemprice.length;i++){
-                itemamount[i].innerText = 'HK$'+(itemprice[i].value)*(itemqty[i].value);
-                subTotal+=(itemprice[i].value)*(itemqty[i].value);
-            }
-            document.getElementById('subTotalSpan').innerText = subTotal;
-        }
-
-
-        function getValue(itemid,qty){
-
-            var qtyChange = {
-                "itemid": itemid,
-                "qty": qty.value};
-            console.log(qtyChange)
-
-            // $.ajax(
-            //     {
-            //     url: 'placeOrder.php',
-            //     data: qtyChange,
-            //     type: 'post',
-            //         success:function (result){
-            //
-            //
-            //         }
-            // })
-        }
-
-    </script>
-
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Better Limited Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark ">
+        <div class="container">
+            <a class="navbar-brand fw-semibold" href="#"
+                style="letter-spacing: 0.05ch">
+                <img src="./images/logo_notext.png" alt="" width="35" height="30" class="d-inline-block align-bottom"
+                    style="object-fit: scale-down">
+                BetterLimited
+            </a>
+            <button class="navbar-toggler" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#toggleHamburger"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="toggleHamburger">
+                <ul class="navbar-nav ms-auto py-3 py-md-0">
+                    <?php
+                        foreach ($urls as $link => $url) {
+                            echo <<<EOD
+                                <li class="nav-item">
+                                    <a href="$url" class="nav-link">$link</a> 
+</li>
+EOD;
 
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-warning">
-
-    <div class="container-fluid">
-
-        <!--LOGO IMAGE-->
-        <a class="navbar-brand" href="#"><img src="images/bltdLogo.png" class="w-100" alt=""></a>
-
-        <ul class="navbar-nav ms-auto mb-0 mb-lg-o">
-
-            <!--Make Order-->
-            <li class="nav-item ">
-                <a class="nav-link h5 text-white" href="#">Make Order</a>
-            </li>
-            <!--View Order-->
-            <li class="nav-item">
-                <a class="nav-link h5 text-white" href="#">View Order</a>
-            </li>
-            <!--Items-->
-            <li class="nav-item">
-                <a class="nav-link h5 text-white" href="#">Items</a>
-            </li>
-            <!--Report-->
-            <li class="nav-item">
-                <a class="nav-link h5 text-white" href="#">Report</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-<!-- Navbar -->
+                        }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+            crossorigin="anonymous"></script>
+</body>
+</html>
